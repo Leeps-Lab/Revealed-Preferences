82f8899ed4b078565cdc009fab57e99169c98ad4
Curtis Kephart <curtisesjunk@gmail.com>
2016-04-19 14:21:32 -0700

    update tatonnement.js - add doc

--- tatonnement/tatonnement.js
+++ tatonnement/tatonnement.js
@@ -52,15 +52,15 @@ RedwoodRevealedPreferences.factory("RPTatonnement", function () {
                                   // in which price are off the grid AND
                                   // the end of the _weightVector has been reached
 
-        var _weightVector = config.weightVector;
-        var _expectedExcess = config.expectedExcess;
+        var _weightVector = config.weightVector; ////R:var weightVector = `k`, k-vector
+        var _expectedExcess = config.expectedExcess; //R:var expectedExcess = `ez`
         
-        var _priceLowerBound = config.priceLowerBound;
-        var _priceUpperBound = config.priceUpperBound;
-        var _maxAngularDiff = config.maxAngularDiff;
+        var _priceLowerBound = config.priceLowerBound; // if not set in config, deaults to 0.1
+        var _priceUpperBound = config.priceUpperBound; // if not set in config, defaults to 100.0
+        var _maxAngularDiff = config.maxAngularDiff;   //if not set in config, defaults to  0.26175
 
-        var _priceGrid = config.priceGrid;
-        var _snapPriceToGrid = config.snapPriceToGrid;
+        var _priceGrid = config.priceGrid; //R:priceGrid = pr
+        var _snapPriceToGrid = config.snapPriceToGrid; //R:snapPriceToGrid = snap  #While this variable =1, snap price to the grid
 
         var priceSnappedToGrid = function(price) {
             return _priceGrid.sort(function(gridPrice1, gridPrice2) {
@@ -101,7 +101,7 @@ RedwoodRevealedPreferences.factory("RPTatonnement", function () {
             var adjustedPrice;
             var excessDemandSign = sign(roundContext.excessDemand);
 
-            var weight = _weightVector[_weightIndex] / _expectedExcess;
+            var weight = _weightVector[_weightIndex] / _expectedExcess;  //R:var expectedExcess = `ez`
                 
             // make sure angular difference is no more than 15 degrees
             var angularDiff = weight * roundContext.excessDemandPerCapita;
