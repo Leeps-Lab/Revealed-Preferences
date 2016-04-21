64fc482fb96c4972657b8ce465142dd2f90e7ef5
Emily <echockel@ucsc.edu>
2016-04-21 13:33:57 -0700

    Back to state at 58351f1

--- plot/rpPlot.js
+++ plot/rpPlot.js
@@ -20,7 +20,6 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
             labelY: "=",
             width: "=",
             height: "=",
-            points: "="
          },
          templateUrl: '/static/experiments/redwood-revealed-preferences/plot/rpPlot.html',
          link: function ($scope, $elem, attr) {
@@ -102,7 +101,7 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
                     .y(function(d) {
                         return yScale(d[1])
                     })
-
+                    
                 var path = plot.select("." + className);
                 if (path.empty()) {
                     plot.append("path")
@@ -116,7 +115,6 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
 
             var drawPoint = function (point, className) {
                 if (!xScale) return;
-                console.log(point);
 
                 var dot = plot.select("." + className);
                 if (dot.empty()) {
@@ -135,7 +133,7 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
 
             var drawBudgetLine = function () {
                 if (!$scope.budgetFunc) return;
-
+                
                 // constrain line to plot boundaries
                 var pointA = [0, $scope.budgetFunc(0)];
                 if (pointA[1] > $scope.limits.y) {
@@ -169,18 +167,6 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
                         "left": toPx(pointRect.left+pointRect.width)
                     });
                 }
-            }
-
-            var drawPoints = function() {
-              console.log("in points");
-              console.log($scope.points);
-              if (!$scope.points) return;
-              console.log("past check");
-
-              $scope.points.forEach(function(ele) {
-                drawPoint(ele, "setpoint");
-                console.log("drew point : "+ele);
-              });
             }
 
             var drawCursor = function () {
@@ -253,7 +239,7 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
             var drawResult = function () {
                 if (!$scope.result || !xScale) return;
 
-                $scope.resultPoint = $scope.result.chosen === "x" ?
+                $scope.resultPoint = $scope.result.chosen === "x" ? 
                     [$scope.result.x, 0] : [0, $scope.result.y];
 
                 drawPoint($scope.resultPoint, "result-point");
@@ -280,7 +266,6 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
                 drawAxes();
                 drawBudgetLine();
                 drawEndowment();
-                drawPoints();
                 drawConstraintPoints();
                 drawSelection();
                 drawCursor();
@@ -337,14 +322,12 @@ RedwoodRevealedPreferences.directive("rpPlot", function ($window) {
             angular.element($window).bind('resize', function() {
                 drawSelection();
                 drawEndowment();
-                drawPoints();
                 drawResult();
             });
 
             angular.element($window).bind('scroll', function() {
                 drawSelection();
                 drawEndowment();
-                drawPoints();
                 drawCursor();
                 drawResult();
             });
